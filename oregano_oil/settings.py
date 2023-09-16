cc1010b5e07efc82630d22ef7077b1f4048c9f19
Holly Williams <hollyyeh12@gmail.com>
2023-09-16 13:26:28 +0000

    add urls and views for homepage

--- oregano_oil/settings.py
+++ oregano_oil/settings.py
@@ -58,7 +58,10 @@ ROOT_URLCONF = 'oregano_oil.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
