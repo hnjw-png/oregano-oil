ac32c60c9684386f65a4e96e7b7e317718d71b49
Holly Williams <hollyyeh12@gmail.com>
2023-09-16 14:48:14 +0000

    add urls for home app

--- oregano_oil/urls.py
+++ oregano_oil/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('index.html')),
+    path('', include('index.urls')),
 ]
