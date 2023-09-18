b708f7a3f98b58ca20cd3d91c112904ba388023c
Holly Williams <hollyyeh12@gmail.com>
2023-09-18 20:22:53 +0000

    add model and admin sections for categories and products

--- products/models.py
+++ products/models.py
@@ -10,14 +10,17 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_names='products', on_delete=models.CASCADE)
-    name = models.Charfield(max_length=255)
+    category = models.ForeignKey(Category, related_name='Product', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
-    price = models.IntegerField(auto_now_add=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 
     class Meta:
         ordering = ('-created_at',)
 
     def _str_(self):
         return self.name
+
