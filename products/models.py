6171dd27555c19c0a4cc409ce6c2bf323d564b0a
Holly Williams <hollyyeh12@gmail.com>
2023-09-18 20:06:15 +0000

    add products model

--- products/models.py
+++ products/models.py
@@ -8,3 +8,16 @@ class Category(models.Model):
 
     def _str_(self):
         return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_names='products', on_delete=models.CASCADE)
+    name = models.Charfield(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def _str_(self):
+        return self.name
