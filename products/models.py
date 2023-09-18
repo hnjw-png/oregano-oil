7679c437fecaa98b74f6cbbc268cbd08486b8cad
Holly Williams <hollyyeh12@gmail.com>
2023-09-18 19:32:51 +0000

    add model to add products to my categories

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+    slug = models.SlugField()
+
+    class Meta: ordering = ('name',)
+
+    def _str_(self):
+        return self.name
