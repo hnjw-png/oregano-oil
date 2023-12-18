eab60d4008a432139d503e014df5337b7dc3a8ce
Holly Williams <hollyyeh12@gmail.com>
2023-12-18 15:15:09 +0000

    add more from info to view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
