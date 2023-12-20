c60500875a1ee91bbc3323b2a68b9f6aa6a5fa19
Holly Williams <hollyyeh12@gmail.com>
2023-12-20 11:44:39 +0000

    add product view and images url

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
-
+    path('product/', product_view, name='product'),
+    path('images/', images_by_product_name, name='images_by_product_name'),
 
 ]
 
