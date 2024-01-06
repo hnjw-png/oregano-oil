b1e0459a708f049f168aed01cca8e15b823e4b8b
Holly Williams <hollyyeh12@gmail.com>
2024-01-06 13:03:39 +0000

    add to profiles app views for profile form.

--- profiles/views.py
+++ profiles/views.py
@@ -1,46 +1,19 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
-
-from .models import UserProfile
-from .forms import UserProfileForm
-
-from checkout.models import Order
-
-def profile(request):
-    """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
-
+def add_product(request):
+    """ Add a product to the store """
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
-
-    template = 'profiles/profile.html'
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
     context = {
         'form': form,
-        'orders': orders,
-        'on_profile_page': True
-    }
-
-    return render(request, template, context)
-
-
-def order_history(request, order_number):
-    order = get_object_or_404(Order, order_number=order_number)
-
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
-    ))
-
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
-        'from_profile': True,
     }
 
     return render(request, template, context)
