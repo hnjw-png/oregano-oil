689a75bc6d3402317b9fc0ab0f25e82f8fb87dab
Holly Williams <hollyyeh12@gmail.com>
2024-01-06 19:54:43 +0000

    add model testimonials.

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Testimonials(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    Product = models.foreignKey(Product, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+        
+
+    def _str_(self):
+        return self.name
+
+        
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Testimonials
+from .forms import TestimonialsForm
+
+@login_required
+def Testimonials(request):
+    """ Add a Testimonial to the store """
+    if request.method == 'POST':
+        form = TestimonialsForm(request.POST, request.FILES)
+        if form.is_valid():
+            Testimonials = form.save()
+            messages.success(request, 'Successfully added Testimonial!')
+            return redirect(reverse('testimonial', args=[Testimonials.id]))
+        else:
+            messages.error(request, 'Failed to add Testimonial. Please ensure the form is valid.')
+    else:
+        form = TestimonialsForm()
+        
+    template = 'testimonials/add_testimonial.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
