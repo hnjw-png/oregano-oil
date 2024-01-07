10950f3dd877d2ce4b23cbeb52bd874118f4f94c
Holly Williams <hollyyeh12@gmail.com>
2024-01-07 16:25:53 +0000

    style testimonials htmls with bootstrap.

--- testimonials/views.py
+++ testimonials/views.py
@@ -13,20 +13,20 @@ class TestimonialsListView(ListView):
 class TestimonialsCreateView(CreateView):
     model = Testimonials
     form_class = TestimonialsForm
-    template_name = 'Testimonials/forms.py'  # Create this template for the event creation form
-    success_url = reverse_lazy('event_list')  # Redirect to the event list after successful creation
+    template_name = 'Testimonials/forms.py'  # Create this template for the testimonial creation form
+    success_url = reverse_lazy('event_list')  # Redirect to the testimonial list after successful creation
 
 @login_required
 class EventUpdateView(UpdateView):
     model = Testimonials
     form_class = TestimonialsForm
-    template_name = 'testimonial/testimonial_form.html'  # Create this template for the event update form
-    context_object_name = 'Testimonial'  # Set the context object name
-    success_url = reverse_lazy('list_testimonials')  # Redirect to the event list after successful update
+    template_name = 'testimonial/testimonial_form.html'  # Create this template for the testimonial update form
+    context_object_name = 'Testimonial'  # Set the testimonial name
+    success_url = reverse_lazy('list_testimonials')  # Redirect to the testimonial list after successful update
 
 @login_required
 class EventDeleteView(DeleteView):
     model = Event
-    template_name = 'testimonial/testimonial_confirm_delete.html'  # Create this template for the delete confirmation page
+    template_name = 'testimonial/testimonial_confirm_delete.html'  # Create this template for the delete a testimonial page
     context_object_name = 'testimonial'  # Set the context object name
-    success_url = reverse_lazy('list_testimonial')  # Redirect to the event list after successful deletion
+    success_url = reverse_lazy('list_testimonial')  # Redirect to the testimonial list after successful deletion
