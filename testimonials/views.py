6a4f8d651b01ee0e904cc77c083ef68a74da8699
Holly Williams <hollyyeh12@gmail.com>
2024-01-07 16:24:04 +0000

    add testimonials views.py for viewing and editting testimonials.

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,29 +1,32 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.db.models.functions import Lower
+from .models import Event
+from .forms import EventForm
 
-from .models import Testimonials
-from .forms import TestimonialsForm
+class TestimonialsListView(ListView):
+    model = Testimonials
+    template_name = 'testimonials/list_testimonials.html'  # Create this template to display the list of events
+    context_object_name = 'events'
 
 @login_required
-def add_testimonial(request):
-    """ Add a Testimonial to the store """
-    if request.method == 'POST':
-        form = TestimonialsForm(request.POST, request.FILES)
-        if form.is_valid():
-            Testimonials = form.save()
-            messages.success(request, 'Successfully added Testimonial!')
-            return redirect(reverse('testimonial', args=[Testimonials.id]))
-        else:
-            messages.error(request, 'Failed to add Testimonial. Please ensure the form is valid.')
-    else:
-        form = TestimonialsForm()
-        
-    template = 'testimonials/add_testimonial.html'
-    context = {
-        'form': form,
-    }
+class TestimonialsCreateView(CreateView):
+    model = Testimonials
+    form_class = TestimonialsForm
+    template_name = 'Testimonials/forms.py'  # Create this template for the event creation form
+    success_url = reverse_lazy('event_list')  # Redirect to the event list after successful creation
 
-    return render(request, template, context)
+@login_required
+class EventUpdateView(UpdateView):
+    model = Testimonials
+    form_class = TestimonialsForm
+    template_name = 'testimonial/testimonial_form.html'  # Create this template for the event update form
+    context_object_name = 'Testimonial'  # Set the context object name
+    success_url = reverse_lazy('list_testimonials')  # Redirect to the event list after successful update
+
+@login_required
+class EventDeleteView(DeleteView):
+    model = Event
+    template_name = 'testimonial/testimonial_confirm_delete.html'  # Create this template for the delete confirmation page
+    context_object_name = 'testimonial'  # Set the context object name
+    success_url = reverse_lazy('list_testimonial')  # Redirect to the event list after successful deletion
