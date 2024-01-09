556c1d961b8565d0b64b55930859b806233d072d
Holly Williams <hollyyeh12@gmail.com>
2024-01-09 20:27:43 +0000

    add views.lpy and urls.py to contact app.´

--- contact/forms.py
+++ contact/forms.py
@@ -5,4 +5,3 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
--- contact/views.py
+++ contact/views.py
@@ -4,16 +4,18 @@ from django.core.mail import send_mail
 from .models import contact
 from .forms import ContactForm
 
-if form.is_valid():
-    subject = form.cleaned_data["What's your question?"]
-    message = form.cleaned_data["Write your message here."]
-    sender = form.cleaned_data["sender"]
-    cc_myself = form.cleaned_data["cc_myself"]
+def Contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+    if form.is_valid():
+       subject = form.cleaned_data["What's your question?"]
+       message = form.cleaned_data["Write your message here."]
+       sender = form.cleaned_data["sender"]
 
-    recipients = ["hollyyeh12@gmail.com"]
-    if cc_myself:
-        recipients.append(sender)
+       recipients = ["hollyyeh12@gmail.com"]
 
-    send_mail(subject, message, sender, recipients)
-    return messages.success(request, 'Contacted!')
-# Create your views here.
+       send_mail(subject, message, sender, recipients)
+       return messages.success(request, 'Contacted!')
+    else:
+        form = ContactForm()
+    return render(request, 'contact_form.html', {'form': form})
