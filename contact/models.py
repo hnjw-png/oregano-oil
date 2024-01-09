8b19b3d65babaa21b5a2b1b14074c36054da8201
Holly Williams <hollyyeh12@gmail.com>
2024-01-09 20:32:38 +0000

    add models.py to contact app.

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    subject = models.CharField(max_length=200)
+    message = models.CharField(max_length=500, default="message")
+    sender = models.CharField(max_length=100)
+
+def __str__(self):
+        return self.sender
