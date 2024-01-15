2de5e460b8c35092ff791a5f174a68a79bf0ec34
Holly Williams <hollyyeh12@gmail.com>
2024-01-15 15:18:44 +0000

    add successful contact html.

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.contrib import messages
 
 from .models import Contact
 from .forms import ContactForm
@@ -11,12 +12,13 @@ def Contact(request):
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             sender = form.cleaned_data['sender']
-            email =form.cleaned_date['email']
+            email =form.cleaned_data['email']
 
             recipients = ["yayogronstroms@gmail.com"]
 
             send_mail(subject, message, sender, recipients)
-            return messages.success(request, 'Contacted!')
+            
+            return render(request, 'contacted.html')
     else:
         form = ContactForm()
     return render(request, 'contact_form.html', {'form': form})
