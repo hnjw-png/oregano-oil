f28e844a23e70cba604b4a9c9b2fb6b97329d49d
Holly Williams <hollyyeh12@gmail.com>
2024-01-15 22:57:22 +0000

    update checkout app

--- profiles/models.py
+++ profiles/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
