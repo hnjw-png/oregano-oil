880b8dcd3b2b13dab6a97b9b9b75aaf47829ed27
Holly Williams <hollyyeh12@gmail.com>
2024-01-19 17:58:09 +0000

    add cloudinary for static files

--- oregano_oil/settings.py
+++ oregano_oil/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage'
     'django.contrib.staticfiles',
+    'cloudinary'
     'django.contrib.sites',
     'allauth',
     'allauth.account',
