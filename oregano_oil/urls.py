412aea7bf22dec1cc14e120434963ce5907742c9
Holly Williams <hollyyeh12@gmail.com>
2024-01-21 15:34:59 +0000

    add 404 errors page

--- oregano_oil/urls.py
+++ oregano_oil/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,3 +20,5 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('info/', include('info.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'oregano_oil.views.handler404'
