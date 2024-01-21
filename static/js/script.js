7e98faa46c5e0521c74a2e9e780ee3577792c36e
Holly Williams <hollyyeh12@gmail.com>
2024-01-21 22:45:11 +0000

    begin to add event listeners to javscript file for interactive stars

--- static/js/script.js
+++ static/js/script.js
@@ -8,9 +8,6 @@ const four = document.getElementById('fourth')
 const five = document.getElementById('fifth')
 
 const form = document.querySelector('rate-form')
-const confirmbox = document.getElementById('confirm-box')
-
-const csrf = document.getElementById('csrfmiddlewaretoken')
 
 const handleStarSelect = (size) => {
     const children = form.children
@@ -52,44 +49,3 @@ const handleSelect = (selection) =>{
     }
 }
 
-const getNumberValue = (stringValue) =>{
-    let NumberValue;
-    if (stringValue === 'first') {
-        NumberValue = 1
-    }
-    else if (stringValue == 'second') {
-        NumberValue = 2
-    }
-    else if (stringValue == 'third') {
-        NumberValue = 3
-    }
-    else if (stringValue == 'fourth') {
-        NumberValue = 4
-    }
-    else if (stringValue == 'fifth') {
-        NumberValue = 5 
-    }
-    else {
-        NumberValue = 0
-    }
-}
-if(one) {
-const arr = [one, two, three, four, five]
-
-arr.forEach(item> item.addEventListener('mouseover', (event)=>{
-    handleSelect(event.target.id)
-}))
-
-arr.forEach(item> item.addEventListener('click', (event)=>{
-    const val = event.target.id
-
-    form.addEventListener('submit', e=>{
-        e.preventDefault()
-        const id = e.target.id
-        const_val_num = getNumberValue(val)
-
-
-    })
-}))
-
-}
